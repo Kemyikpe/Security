1438363b10acdf24ba2ac86892e27fb03316b17f
samson akisanya <43546652+uticodes@users.noreply.github.com>
2023-04-11 10:07:11 +0100

    implement onboarding

--- app/src/main/java/com/starthub/securityApp/CustomDialogFragment.kt
+++ app/src/main/java/com/starthub/securityApp/CustomDialogFragment.kt
@@ -5,13 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class CustomDialogFragment :DialogFragment() {
+class CustomDialogFragment :GuardSessionFragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var rootView:View =inflater.inflate(R.layout.fragment_dialog,container,false)
+        var rootView:View =inflater.inflate(R.layout.fragment_guard_seesion,container,false)
         return rootView
     }
 }
--- app/src/main/java/com/starthub/securityApp/GuardSessionFragment.kt
+++ app/src/main/java/com/starthub/securityApp/GuardSessionFragment.kt
@@ -6,14 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.navigation.fragment.findNavController
-import com.starthub.securityApp.databinding.DiaglogBinding
-import com.starthub.securityApp.databinding.FragmentDialogBinding
-import com.starthub.securityApp.databinding.FragmentGetStartedBinding
+import com.starthub.securityApp.databinding.FragmentGuardSeesionBinding
 
 
-open class DialogFragment : Fragment() {
-    private var binding: FragmentDialogBinding? = null
+open class GuardSessionFragment : Fragment() {
+    private var binding: FragmentGuardSeesionBinding? = null
 
 
 
@@ -22,8 +19,8 @@ open class DialogFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentDialogBinding.inflate(inflater,container,false)
-        val view = inflater.inflate(R.layout.fragment_dialog,container,false)
+        binding = FragmentGuardSeesionBinding.inflate(inflater,container,false)
+        val view = inflater.inflate(R.layout.fragment_guard_seesion,container,false)
          val button = view.findViewById<Button>(R.id.btn1)
         button .setOnClickListener {
             var dialog = CustomDialogFragment()
